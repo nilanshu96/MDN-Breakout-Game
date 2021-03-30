5e9f7eb155e06e55f7f0f7e9134f56ab8dc788e9
Nilanshu V Rajmane <rajmanenilanshu@gmail.com>
2021-03-30 14:32:41 +0530

    added bricks

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ function drawBall(x, y, radius) {
   ctx.closePath();
 }
 
+
 let paddleHeight = 10;
 let paddleWidth = 75;
 let paddleX = (canvas.width - paddleWidth) / 2;
@@ -34,9 +35,42 @@ function drawPaddle() {
   ctx.closePath();
 }
 
+let brickRowCount = 3;
+let brickColumnCount = 5;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
+
+let bricks = [];
+for (let r = 0; r < brickRowCount; r++) {
+  bricks[r] = [];
+  for (let c = 0; c < brickColumnCount; c++) {
+    bricks[r][c] = { x: 0, y: 0 };
+  }
+}
+
+function drawBricks() {
+
+  for(r=0; r<brickRowCount; r++)
+    for(c=0; c<brickColumnCount; c++) {
+      let brickX = c*(brickWidth + brickPadding) + brickOffsetLeft;
+      let brickY = r*(brickHeight + brickPadding) + brickOffsetTop;
+      bricks[r][c].x = brickX;
+      bricks[r][c].y = brickY;
+      ctx.beginPath();
+      ctx.rect(brickX, brickY, brickWidth, brickHeight);
+      ctx.fillStyle = "#0095DD";
+      ctx.fill();
+      ctx.closePath();
+    }
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  drawBricks();
   drawBall(x, y, ballRadius);
   drawPaddle();
 
